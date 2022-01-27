dafbb4cd19810dfa530bf3ac62f1c407b99a0f83
mhmmdkfh <muhammad.kifahi95@gmail.com>
2022-01-27 15:48:41 +0700

    edit get cart

--- Tokopodia/GraphQL/Queries/CartQuery.cs
+++ Tokopodia/GraphQL/Queries/CartQuery.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Tokopodia.Data;
+using Tokopodia.Data.BuyerProfiles;
+using Tokopodia.Data.Users;
+using Tokopodia.Helper;
 using Tokopodia.Models;
 
 namespace Tokopodia.GraphQL.Queries
@@ -16,20 +19,48 @@ namespace Tokopodia.GraphQL.Queries
     {
         public async Task<IQueryable<Cart>> GetAllCartsByBuyer(
             [Service] AppDbContext context,
+            [Service] IUser user,
+            [Service] IBuyerProfile buyerProfile,
             [Service] IHttpContextAccessor httpContextAccessor)
         {
-            var buyerId = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            var carts = context.Carts.Where(c => c.BuyerId == Convert.ToInt32(buyerId));
+            var userId = httpContextAccessor.HttpContext.User.FindFirst("UserId").Value;
+            if (userId == null)
+            {
+                throw new Exception("Unauthorized.");
+            }
+
+            var userResult = await user.GetById(userId);
+            if (userResult == null)
+                throw new UserNotFoundException();
+            var buyerResult = await buyerProfile.GetByUserId(userResult.Id);
+            if (buyerResult == null)
+                throw new UserNotFoundException();
+
+            var carts = context.Carts.Where(c => c.BuyerId == buyerResult.Id && c.Status == "OnTransaction");
 
             return carts; 
         }
 
         public async Task<IQueryable<Cart>> GetCartOnCartBuyer(
             [Service] AppDbContext context,
+            [Service] IUser user,
+            [Service] IBuyerProfile buyerProfile,
             [Service] IHttpContextAccessor httpContextAccessor)
         {
-            var buyerId = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            var cart = context.Carts.Where(c => c.BuyerId == Convert.ToInt32(buyerId) && c.Status == "OnCart");
+            var userId = httpContextAccessor.HttpContext.User.FindFirst("UserId").Value;
+            if (userId == null)
+            {
+                throw new Exception("Unauthorized.");
+            }
+
+            var userResult = await user.GetById(userId);
+            if (userResult == null)
+                throw new UserNotFoundException();
+            var buyerResult = await buyerProfile.GetByUserId(userResult.Id);
+            if (buyerResult == null)
+                throw new UserNotFoundException();
+
+            var cart = context.Carts.Where(c => c.BuyerId == buyerResult.Id && c.Status == "OnCart");
 
             return cart;
         }
