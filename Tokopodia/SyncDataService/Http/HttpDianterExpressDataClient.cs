a6fe78ea54f59b4e236cc1ac10b79e8becad3d99
rahmatalhakam <rahmatalhakam@gmail.com>
2022-01-29 19:06:50 +0700

    add token for diantar authorization

--- Tokopodia/SyncDataService/Http/HttpDianterExpressDataClient.cs
+++ Tokopodia/SyncDataService/Http/HttpDianterExpressDataClient.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
 using Microsoft.Extensions.Options;
 using HotChocolate;
 using Tokopodia.SyncDataService.Dtos;
@@ -23,6 +23,8 @@ namespace Tokopodia.SyncDataService.Http
       HttpClientHandler handler = new HttpClientHandler();
       using (var client = new HttpClient(handler, false))
       {
+        client.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlNlcnZpY2VzIiwicm9sZSI6IlRva29wb2RpYSIsIm5iZiI6MTY0MzQ1NTM3NCwiZXhwIjoxNjU5MDkzNzc0LCJpYXQiOjE2NDM0NTUzNzR9.75tzAOdYRqACuZj_Xq7TLmAUVde9heaYvZMkl_ECcuk");
         var json = JsonSerializer.Serialize(input);
         var data = new StringContent(json, Encoding.UTF8, "application/json");
         var response = await client.PostAsync(_appSettings.DiantarExpressUrl + "/api/v1/Shipment/tokopodia", data);
@@ -42,6 +44,8 @@ namespace Tokopodia.SyncDataService.Http
       using (var client = new HttpClient(handler, false))
       {
         var json = JsonSerializer.Serialize(input);
+        client.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlNlcnZpY2VzIiwicm9sZSI6IlRva29wb2RpYSIsIm5iZiI6MTY0MzQ1NTM3NCwiZXhwIjoxNjU5MDkzNzc0LCJpYXQiOjE2NDM0NTUzNzR9.75tzAOdYRqACuZj_Xq7TLmAUVde9heaYvZMkl_ECcuk");
         var data = new StringContent(json, Encoding.UTF8, "application/json");
         var response = await client.PostAsync(_appSettings.DiantarExpressUrl + "/api/v1/Shipment/fee", data);
         var content = await response.Content.ReadAsStringAsync();
