e34e20750b5ffd69a2e9829231a022ed5ed9ee34
Rizqul Akbar <rizqul141@gmail.com>
2022-01-30 11:25:00 +0700

    Fix Seller Query

--- Tokopodia/GraphQL/Queries/SellerProfileQuery.cs
+++ Tokopodia/GraphQL/Queries/SellerProfileQuery.cs
@@ -2,12 +2,17 @@ using System;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using AutoMapper;
 using HotChocolate;
+using HotChocolate.AspNetCore.Authorization;
 using HotChocolate.Types;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Tokopodia.Data;
+using Tokopodia.Data.SellerProfiles;
+using Tokopodia.Data.Users;
+using Tokopodia.Helper;
 using Tokopodia.Helpers;
 using Tokopodia.Output;
 
@@ -17,31 +22,31 @@ namespace Tokopodia.GraphQL.Queries
   [System.Obsolete]
   public class SellerProfileQuery
     {
-        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
         private readonly IHttpContextAccessor _httpContextAccessor;
-
-        public SellerProfileQuery(AppDbContext context,
-                                 IHttpContextAccessor httpContextAccessor)
+        public SellerProfileQuery([Service] IMapper mapper, [Service] IHttpContextAccessor httpContextAccessor)
         {
-            _context = context;
+            _mapper = mapper;
             _httpContextAccessor = httpContextAccessor;
         }
 
-        public SellerOutput SellerProfile()
+        [Authorize(Roles = new[] { "Seller" })]
+        public async Task<SellerOutput> GetBuyerProfile([Service] IUser _user, [Service] ISellerProfile _buyerProfile)
         {
-            var userName = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Name).Value;
-            if (userName == null)
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst("UserId").Value;
+            if (userId == null)
             {
-                throw new Exception("Silahkan Login Terlebih Dahulu");
+                throw new Exception("Unauthorized.");
             }
-            return (SellerOutput)_context.SellerProfiles.Select(seller => new SellerOutput()
-            {
-                Id = Convert.ToInt32(seller.UserId),
-                Username = seller.Username,
-                //Address = seller.Address,
-                //ShopName = seller.ShopName,
-                CreatedAt = seller.CreatedAt
-            }).Where(seller => seller.Username == userName);
+            var userResult = await _user.GetById(userId);
+            if (userResult == null)
+                throw new UserNotFoundException();
+            var profileResult = await _buyerProfile.GetByUserId(userResult.Id);
+            if (profileResult == null)
+                throw new UserNotFoundException();
+            var sellerOutput = _mapper.Map<SellerOutput>(profileResult);
+            sellerOutput.userOutput = _mapper.Map<UserOutput>(profileResult.User);
+            return sellerOutput;
         }
 
 
